e0c925c688f45fba02f166e5ce7052f08ae34933
Sirivat Asanasuwan <sirivatasa@outlook.de>
2023-10-26 20:52:48 +0200

    Project cleanup. Add tailwind and skeleton css

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,18 +4,54 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>About This App</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
+		This is a simple weather app on top of an overly complicated and overengineered infrastructure. 
+		I am not joking, its really meme worthy level of overengineering. 🙃
 	</p>
 
-	<pre>npm create svelte@latest</pre>
-
+	<h3>The Frontend Fiesta</h3>
 	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
+		The application was written in Svelte and SvelteKit. It is built via Static Site Generation (SSG) 
+		and deployed to AWS S3 which serves as a Static Website Host. The application you are looking at is 
+		served via AWS Cloudfront which is a Content Delivery Network (CDN) that caches the application and
+		provides a global edge location for the application to be served from.
+	</p>
+
+	<h3>The Behind-The-Scene-Backend</h3>
+	<p>
+		The application communicates via a REST API written in Go that runs on AWS Apprunner. Apprunner takes
+		the image that is built and pushed to AWS Elastic Container Registry (ECR) from Github Actions and
+		runs it as a container, it is a fully managed serverless solution by AWS. The API is a simple Go application
+		that has multiple endpoints to get weather data from DynamoDB. Ain't gonna lie, it's blazingly fast.
+	</p>
+
+	<h3>The Closet In the Backend Leading To Narnia</h3>
+	<p>
+		DynamoDB serves as the database for the application. There are multiple tables that store the weather data
+		for different cities. The data is populated via a Lambda function (written in NodeJS) that is triggered by EventBridge. 
+		EventBridge, this basically turns our Lambda function into a cron job that runs periodically to fetch the 
+		weather data from WeatherApi.
+	</p>
+
+	<h3>The Meme Worty Overengineering</h3>
+	<p>
+		This is the part where I tell you, that this whole project, the application you are looking at, the API, 
+		the database, literally everything is running on AWS provided and configured with Terraform, not just
+		Terraform, to make it even more complicated, it's Terragrunt. Makes life easier when you have multiple
+		environments. Yes, the whole infrastructure is seperated into development, staging / non-production,
+		and production and shared-resources. Now you might be thinking, why would you do this? 
+	</p>
+	<p>
+		Well, just because I can. And maybe because I might be a psychopath who happens to be overly charming
+		and handsome as well. And definitely not because I'm desperate to show off my skills for a job so I 
+		can pay my OnlyFans subscriptions.
 	</p>
 </div>
+
+<style>
+	h3 {
+		margin-bottom: 0;
+	}
+</style>
