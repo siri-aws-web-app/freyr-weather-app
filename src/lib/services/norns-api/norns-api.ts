e9801207156bf1ebed1763c783f3554179475a41
Sirivat Asanasuwan <sirivatasa@outlook.de>
2023-11-01 17:36:08 +0100

    remove env specific config from gitignore

--- src/lib/services/norns-api/norns-api.ts
+++ src/lib/services/norns-api/norns-api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-console.log(import.meta.env.VITE_NORNS_API_URL);
 axios.defaults.baseURL = import.meta.env.VITE_NORNS_API_URL;
 
 export function getCurrentPastWeatherData(cities: string[]) {
