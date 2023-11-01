19edce6c51ea2f00d5768de3ab70cd1ed99c81af
Sirivat Asanasuwan <sirivatasa@outlook.de>
2023-11-01 19:34:46 +0100

    refactor code, add dashboard for weather

---
+++ src/lib/components/weather-dashboard/weather-dashboard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import SvelteHead from '$lib/components/header/svelte-head.svelte';
+	import SpanGradient from '$lib/components/typography/span-gradient.svelte';
+	import { fade } from 'svelte/transition';
+</script>
+
+<SvelteHead city="About" metaName="about" metaContent="About this app" />
+
+<article class="prose prose-xl prose-invert mx-auto my-8" in:fade={{ duration:400 }}>
+	<h1 class="text-center text">
+		<SpanGradient text="Karlsruhe" />
+	</h1>
+</article>
