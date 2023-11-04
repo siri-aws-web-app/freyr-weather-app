0571af528ef4103960a567c2e486094643700c41
Sirivat Asanasuwan <sirivatasa@outlook.de>
2023-11-04 02:29:28 +0100

    Add image loading state for smoother transitions

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,5 +2,5 @@
 // it so that it gets served as a static asset in production
 export const prerender = true;
 
-// Fetch current weather data on page load and save to store
-// getAndSaveCurrentWeatherData();
+// Turn off server-side rendering
+// export const ssr = false;
