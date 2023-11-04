625a4be5133ca2fee41099502029b0888c710a39
Sirivat Asanasuwan <sirivatasa@outlook.de>
2023-11-04 04:32:37 +0100

    Rearrange cities

--- src/lib/components/img-components/city-cards-flex.svelte
+++ src/lib/components/img-components/city-cards-flex.svelte
@@ -6,14 +6,14 @@
 
     let cities = [
         { name: 'Karlsruhe', image: import('$lib/images/cities/karlsruhe.png') },
-        { name: 'Stuttgart', image: import('$lib/images/cities/stuttgart.png') },
         { name: 'Berlin', image: import('$lib/images/cities/berlin.png') },
         { name: 'Munich', image: import('$lib/images/cities/munich.png') },
-        { name: 'Cologne', image: import('$lib/images/cities/cologne.png') },
         { name: 'Frankfurt', image: import('$lib/images/cities/frankfurt.png') },
         { name: 'Hamburg', image: import('$lib/images/cities/hamburg.png') },
+        { name: 'Cologne', image: import('$lib/images/cities/cologne.png') },
         { name: 'Dusseldorf', image: import('$lib/images/cities/dusseldorf.png') },
         { name: 'Dortmund', image: import('$lib/images/cities/dortmund.png') },
+        { name: 'Stuttgart', image: import('$lib/images/cities/stuttgart.png') },
         { name: 'Leipzig', image: import('$lib/images/cities/leipzig.png') },
     ];
 
