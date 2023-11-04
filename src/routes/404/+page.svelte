1000beeee3ca4700bfc6764f6f999669abde2e45
Sirivat Asanasuwan <sirivatasa@outlook.de>
2023-11-04 02:10:29 +0100

    Add 404

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import SvelteHead from "$lib/components/header/svelte-head.svelte";
+	import { fade } from "svelte/transition";
+</script>
+
+<!-- 404.svelte -->
+<SvelteHead city="404" metaName="NotFound" metaContent="Page not found." />
+
+<div class="container h-full mx-auto flex justify-center items-center">
+    <article class="prose prose-xl prose-invert mx-auto" in:fade={{ duration:300}}>
+		<h1 class="text-center text">
+            It's a 404. Nothing to see here. Move along. 😇
+		</h1>
+    </article>
+</div>
